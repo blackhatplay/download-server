4121380b9cb1219e8c5ad98016c6503e6e015574
Manoj Singh <ext.manoj.s@goodworker.in>
2021-09-17 00:52:40 +0530

    added port to variable

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const { default: axios } = require("axios");
-const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-var cors = require("cors");
+const { default: axios } = require('axios');
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+var cors = require('cors');
 
-const { post } = require("./getLink");
+const { post } = require('./getLink');
 
-const url = "https://getintopc.com/wait-for-resource-5/";
-const gameDownloadUrl =
-  "http://tutorials-forum.info/fast-hosting-file-max-speed-ofg/";
-const urlHeader = "https://httpbin.org/anything";
-const softwareUrl = "https://getintopc.com/";
-const gameUrl = "http://oceanofgames.com/";
+const PORT = 5000;
+const url = 'https://getintopc.com/wait-for-resource-5/';
+const gameDownloadUrl = 'http://tutorials-forum.info/fast-hosting-file-max-speed-ofg/';
+const urlHeader = 'https://httpbin.org/anything';
+const softwareUrl = 'https://getintopc.com/';
+const gameUrl = 'http://oceanofgames.com/';
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
@@ -21,73 +21,73 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 (async () => {
-  const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
 
-  app.use("/download/", async (req, res) => {
-    const page = await browser.newPage();
+    app.use('/download/', async (req, res) => {
+        const page = await browser.newPage();
 
-    await page.goto(req.body.url);
+        await page.goto(req.body.url);
 
-    const content = await page.content();
+        const content = await page.content();
 
-    const $ = cheerio.load(content);
+        const $ = cheerio.load(content);
 
-    const formInputs = [];
+        const formInputs = [];
 
-    $("form", ".post-content").each(function () {
-      const form = $(this).html();
-      const link = {};
+        $('form', '.post-content').each(function () {
+            const form = $(this).html();
+            const link = {};
 
-      $("input", form).each(function () {
-        link[$(this).attr("name")] = $(this).attr("value");
-      });
+            $('input', form).each(function () {
+                link[$(this).attr('name')] = $(this).attr('value');
+            });
 
-      formInputs.push(link);
+            formInputs.push(link);
+        });
+
+        page.on('request', (request) => {
+            if (request.url().search(formInputs[0].filename) > 0) {
+                res.json({ url: request.url(), ...formInputs[0] });
+                page.close();
+            }
+        });
+
+        if (req.body.url.search(gameUrl) > -1) {
+            await post(page, gameDownloadUrl, formInputs[0]);
+        } else if (req.body.url.search(softwareUrl) > -1) {
+            await post(page, url, formInputs[0]);
+        }
     });
 
-    page.on("request", (request) => {
-      if (request.url().search(formInputs[0].filename) > 0) {
-        res.json({ url: request.url(), ...formInputs[0] });
+    app.use('/posts/:type', async (req, res) => {
+        const page = await browser.newPage();
+        if (req.params.type === 'game') {
+            await page.goto(`${gameUrl}?s=${req.query.s}`);
+        } else if (req.params.type === 'software') {
+            await page.goto(`${softwareUrl}?s=${req.query.s}`);
+        }
+
+        const content = await page.content();
+
         page.close();
-      }
+
+        const $ = cheerio.load(content);
+
+        const posts = [];
+
+        $('.post-thumb', '.posts').each(function () {
+            const pp = $(this).html();
+
+            const post = {
+                href: $(this).attr('href'),
+                title: $(this).attr('title'),
+            };
+
+            posts.push(post);
+        });
+
+        res.json(posts);
     });
 
-    if (req.body.url.search(gameUrl) > -1) {
-      await post(page, gameDownloadUrl, formInputs[0]);
-    } else if (req.body.url.search(softwareUrl) > -1) {
-      await post(page, url, formInputs[0]);
-    }
-  });
-
-  app.use("/posts/:type", async (req, res) => {
-    const page = await browser.newPage();
-    if (req.params.type === "game") {
-      await page.goto(`${gameUrl}?s=${req.query.s}`);
-    } else if (req.params.type === "software") {
-      await page.goto(`${softwareUrl}?s=${req.query.s}`);
-    }
-
-    const content = await page.content();
-
-    page.close();
-
-    const $ = cheerio.load(content);
-
-    const posts = [];
-
-    $(".post-thumb", ".posts").each(function () {
-      const pp = $(this).html();
-
-      const post = {
-        href: $(this).attr("href"),
-        title: $(this).attr("title"),
-      };
-
-      posts.push(post);
-    });
-
-    res.json(posts);
-  });
-
-  app.listen(5000, () => console.log("Server is running on port 5000"));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 })();
